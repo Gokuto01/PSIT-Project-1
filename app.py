c4fe3e7204df97f12fe3c153830153820991324c
ChairawichZ6 <70460005+ChairawichZ6@users.noreply.github.com>
2020-12-10 12:25:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS
-from All_Anime_List import test
+from route import get_all, get_name, select_type
+from Google import Create_Service
+import pandas as pd
 
-data = [###รอข้อมูลจาก database
+CLIENT_SECRET_FILE = 'client_secret.json'
+API_SERVICE_NAME = 'sheets'
+API_VERSION = 'v4'
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+gsheetId = '1XTmcMfTa9BLlqXfhvE4wDN0lkMaLkkzP809vb4FLKpw'
+
+s = Create_Service(CLIENT_SECRET_FILE, API_SERVICE_NAME, API_VERSION, SCOPES)
+gs = s.spreadsheets()
+rows = gs.values().get(spreadsheetId=gsheetId,range='Test1').execute()
+data = rows.get('values')
+df = pd.DataFrame(data)
+print(df)
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
@@ -21,16 +34,20 @@ def return_all():
 @app.route('/get_name/<name>')
 def get_name(name):
     return jsonify(get_anime())
-
-@app.route('/name_anime')#name
+    
+@app.route('/select_type/<types>')
+def select_type(types):
+    return jsonify(slect_type())
+    
+@app.route('/name_anime')
 def name_anime(names):
     return jsonify(name_anime())
 
-@app.route('/type_anime/<type>')#type
+@app.route('/type_anime/<type>')
 def type_anime(types):
     return jsonify(type_anime())
 
-@app.route('/link_anime/<type>')#link
+@app.route('/link_anime/<type>')
 def link_anime(link):
     return jsonify(link_anime())
 
