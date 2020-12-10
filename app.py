04ff8a9f4a080d01c4b91adc617d23567a85f5dd
ChairawichZ6 <70460005+ChairawichZ6@users.noreply.github.com>
2020-12-10 17:10:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,9 @@
 from flask import Flask, render_template, jsonify, request
 from flask_cors import CORS
 from route import get_all, get_name, select_type
-from Google import Create_Service
 import pandas as pd
 
-CLIENT_SECRET_FILE = 'client_secret.json'
-API_SERVICE_NAME = 'sheets'
-API_VERSION = 'v4'
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
-gsheetId = '1XTmcMfTa9BLlqXfhvE4wDN0lkMaLkkzP809vb4FLKpw'
-
-s = Create_Service(CLIENT_SECRET_FILE, API_SERVICE_NAME, API_VERSION, SCOPES)
-gs = s.spreadsheets()
-rows = gs.values().get(spreadsheetId=gsheetId,range='Test1').execute()
-data = rows.get('values')
-df = pd.DataFrame(data)
-print(df)
+#เรียกใช้ข้อมูลจาก gsheet
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
