9a431884d01d3eda23f94f7e89210e85132c80d4
ChairawichZ6 <70460005+ChairawichZ6@users.noreply.github.com>
2020-12-10 17:09:07 +0700

    Update data_base.py

--- data_base.py
+++ data_base.py
@@ -1,7 +1,7 @@
 import pandas as pd 
-
+#https://stackoverflow.com/questions/2974022/is-it-possible-to-assign-the-same-value-to-multiple-keys-in-a-dict-object-at-onc
 sheet_id = '1XTmcMfTa9BLlqXfhvE4wDN0lkMaLkkzP809vb4FLKpw'
-dbf = pd.read_csv(f'https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv')
+dbf = pd.read_csv(f'https://docs.google.com/spreadsheets/d/%7Bsheet_id%7D/export?format=csv%27)
 
 def  multi_key_dict_get(d, k):
     for keys, v in d.items():
@@ -10,9 +10,8 @@ def  multi_key_dict_get(d, k):
     return None
 
 anime_dict = {}
-for anime_all in range(50):
-    str_name = dbf.get('Name')[i]
+for i in range(50):
+    str_name = dbf.get('NAME')[i]
     tuple_name = tuple(str_name.split(', '))
-    link_name = dbf.get('Link')[i]
+    link_name = dbf.get('LINK')[i]
     anime_dict.update({tuple_name: link_name})
-print(anime_dict)
