9408c61a7dbd58510d9b37c939be5005e6520233
Gokuto Kung <73678927+Gokuto01@users.noreply.github.com>
2020-12-12 11:53:13 +0700

    Update data_base.py

--- data_base.py
+++ data_base.py
@@ -3,22 +3,6 @@ import pandas as pd
 sheet_id = '1XTmcMfTa9BLlqXfhvE4wDN0lkMaLkkzP809vb4FLKpw'
 df  = pd.read_csv(f'https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv')
 
-def  multi_key_dict_get(d, k):
-    for keys, v in d.items():
-        if k in keys:
-            return v
-    return None
-
-def anime_dict():
-    name_link_anime = {}
-    num_anime = list(df.get('NO.'))[-1]
-    for i in range(num_anime):
-        str_name = df.get('NAME')[i]
-        tuple_name = tuple(str_name.split(', '))
-        link_name = df.get('LINK')[i]
-        name_link_anime.update({tuple_name: link_name})
-    return name_link_anime
-
 def anime_data():
     data=[]
     num_anime = list(df.get('NO.'))[-1]
