4cb8237404a77e46bb51b67ac3908b54f9bf9d54
iioUHoD <41914088+iioUHoD@users.noreply.github.com>
2020-12-12 11:46:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,41 +3,22 @@ from Flask_cors import CORS
 from route import get_all, get_genres, get_name
 from data_base import anime_list
 
-name_link = anime_dict()
-
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 @app.route('/')
-def home():
-    return jsonify(data)
-
-@app.route('/api')
-def api():
-    return jsonify(test())
 
 @app.route('/get_all')
 def return_all():
     return jsonify(get_all())
 
 @app.route('/get_name/<name>')
-def get_name(name):
-    return jsonify(get_anime())
+def return_anime(name):
+    return jsonify(get_anime(name))
     
-@app.route('/select_type/<types>')
-def select_type(types):
-    return jsonify(slect_type())
+@app.route('/get_gen/<type>')
+def return_gen(gen):
+    return jsonify(get_genres(gen))
     
-@app.route('/name_anime')
-def name_anime(names):
-    return jsonify(name_anime())
-
-@app.route('/type_anime/<type>')
-def type_anime(types):
-    return jsonify(type_anime())
-
-@app.route('/link_anime/<type>')
-def link_anime(link):
-    return jsonify(link_anime())
 
 if __name__ == '__main__':
     app.run(debug=True)
