da3ae397ab1e8f8fcb412d400ad45ffdfb710270
Gokuto Kung <73678927+Gokuto01@users.noreply.github.com>
2020-12-12 10:31:21 +0700

    Add files via upload

--- data_base.py
+++ data_base.py
@@ -1,7 +1,7 @@
 import pandas as pd 
 #https://stackoverflow.com/questions/2974022/is-it-possible-to-assign-the-same-value-to-multiple-keys-in-a-dict-object-at-onc
 sheet_id = '1XTmcMfTa9BLlqXfhvE4wDN0lkMaLkkzP809vb4FLKpw'
-dbf = pd.read_csv(f'https://docs.google.com/spreadsheets/d/%7Bsheet_id%7D/export?format=csv%27)
+df  = pd.read_csv(f'https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv')
 
 def  multi_key_dict_get(d, k):
     for keys, v in d.items():
@@ -10,11 +10,11 @@ def  multi_key_dict_get(d, k):
     return None
 
 def anime_dict():
-    anime_dict = {}
-    for i in range(50):
-        str_name = dbf.get('NAME')[i]
+    name_link_anime = {}
+    num_anime = list(df.get('NO.'))[-1]
+    for i in range(num_anime):
+        str_name = df.get('NAME')[i]
         tuple_name = tuple(str_name.split(', '))
-        link_name = dbf.get('LINK')[i]
-        anime_dict.update({tuple_name: link_name})
-    return anime_dict
-
+        link_name = df.get('LINK')[i]
+        name_link_anime.update({tuple_name: link_name})
+    return name_link_anime
