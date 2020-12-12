8a2e20ac40a5fc2fe2febd1f8e7219b0f700de0b
Gokuto Kung <73678927+Gokuto01@users.noreply.github.com>
2020-12-12 14:44:25 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
-from Flask import Flask, jsonify
-from Flask_cors import CORS
-from route import get_all, get_genres, get_name
-from data_base import anime_data
+from flask import Flask, jsonify
+from flask_cors import CORS
+from route import get_all, get_name, get_genres
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
@@ -12,12 +11,12 @@ def return_all():
     return jsonify(get_all())
 
 @app.route('/get_name/<name>')
-def return_anime(name):
-    return jsonify(get_anime(name))
+def return_name(name):
+    return jsonify(get_name(name))
     
 @app.route('/get_genres/<genres>')
 def return_genres(genres):
-    return jsonify(get_genres(gen))
+    return jsonify(get_genres(genres))
     
 
 if __name__ == '__main__':
